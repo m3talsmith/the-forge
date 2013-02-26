b62042f8e70ddba548915a1a350d0c5b8a66f1f0
lordbyron <bryan@deepwoodsbrigade.com>
2013-02-25 21:00:40 -0500

    removed facebook and dwolla auth; fixed google oauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,13 +2,6 @@ provider_keys = YAML.load_file(File.join(Rails.root, 'config', 'provider_keys.ym
 
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
-    :facebook,
-    provider_keys[Rails.env]['facebook']['consumer_key'],
-    provider_keys[Rails.env]['facebook']['consumer_secret'],
-    display: 'popup',
-    scope: 'offline_access'
-  )
-  provider(
     :twitter,
     provider_keys[Rails.env]['twitter']['consumer_key'],
     provider_keys[Rails.env]['twitter']['consumer_secret']
@@ -17,12 +10,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
     :google_oauth2,
     provider_keys[Rails.env]['google']['consumer_key'],
     provider_keys[Rails.env]['google']['consumer_secret']
-  )
-  provider(
-    :dwolla,
-    provider_keys[Rails.env]['dwolla']['consumer_key'],
-    provider_keys[Rails.env]['dwolla']['consumer_secret'],
-    display: 'popup',
-    scope: 'accountinfofull|send|request'
   )
 end
